37dff74cfa71faf67f24725a3fbfa3a8ff1e492f
王鹏飞 <2468852146@qq.com>
2024-09-18 11:30:22 +0800

    11-3 part2 Start

---
+++ Source/Aura/Fura/Data/CharacterClassInfo_F.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright Druid Mechanics
+
+
+#include "CharacterClassInfo_F.h"
+
+FCharacterClassDefaultInfo_F UCharacterClassInfo_F::GetClassDefaultInfo(ECharacterClass_F CharacterClass)
+{
+		return CharacterClassInformation.FindAndRemoveChecked(CharacterClass);
+}
---
+++ Source/Aura/Fura/Data/CharacterClassInfo_F.h
@@ -0,0 +1,49 @@
+﻿// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/Classes/Engine/DataAsset.h"
+#include "CharacterClassInfo_F.generated.h"
+
+class UGameplayEffect;
+
+UENUM()
+enum class ECharacterClass_F : uint8
+{
+	Elementalist,
+	Warrior,
+	Ranger
+};
+
+USTRUCT()
+struct FCharacterClassDefaultInfo_F
+{
+	GENERATED_BODY()
+	UPROPERTY(EditDefaultsOnly, Category="Class Defaults_F")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo_F : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Character Class Defaults_F")
+	TMap<ECharacterClass_F, FCharacterClassDefaultInfo_F> CharacterClassInformation;
+
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults_F")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults_F")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+
+	//返回结构体
+	FCharacterClassDefaultInfo_F GetClassDefaultInfo(ECharacterClass_F CharacterClass);
+};
