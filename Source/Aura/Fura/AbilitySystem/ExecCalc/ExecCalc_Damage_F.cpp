48f47c3efc091ba0b59e8ae3c8666fffc3757988
WETALWAX <2468852146@qq.com>
2024-09-22 17:50:04 +0800

    12-12

---
+++ Source/Aura/Fura/AbilitySystem/ExecCalc/ExecCalc_Damage_F.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright Druid Mechanics
+
+
+#include "ExecCalc_Damage_F.h"
+
+#include "AbilitySystemComponent.h"
+
+void UExecCalc_Damage_F::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+                                                FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+
+	//计算过程中捕获属性
+	//获取效果源和目标AbilitySystemComponent
+	UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	UAbilitySystemComponent* TargetASC =ExecutionParams.GetTargetAbilitySystemComponent();
+	//获取对应的源和目标的Actor
+	AActor* SourceActor=SourceASC?SourceASC->GetAvatarActor():nullptr;
+	AActor* TargetActor=TargetASC?TargetASC->GetAvatarActor():nullptr;
+	//获取gameplay effect spec
+	const FGameplayEffectSpec& Spec=ExecutionParams.GetOwningSpec();
+	
+	
+
+	
+}
---
+++ Source/Aura/Fura/AbilitySystem/ExecCalc/ExecCalc_Damage_F.h
@@ -0,0 +1,27 @@
+﻿// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "ExecCalc_Damage_F.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UExecCalc_Damage_F : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UExecCalc_Damage_F();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+
+
+
+
+
+	
+};
