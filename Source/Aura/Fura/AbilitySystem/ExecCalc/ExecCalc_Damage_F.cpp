32f82dce21aca391c7197ab87c199250f77af94a
王鹏飞 <2468852146@qq.com>
2024-09-23 18:00:27 +0800

    12-14-end

--- Source/Aura/Fura/AbilitySystem/ExecCalc/ExecCalc_Damage_F.cpp
+++ Source/Aura/Fura/AbilitySystem/ExecCalc/ExecCalc_Damage_F.cpp
@@ -5,15 +5,18 @@
 
 #include "AbilitySystemComponent.h"
 #include "Aura/Fura/FuraAttributeSet.h"
+#include "Aura/Fura/FuraGamePlayTags.h"
 
 
 struct FuraDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 
 	FuraDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UFuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UFuraAttributeSet, BlockChance, Target, false);
 	}
 };
 
@@ -28,6 +31,7 @@ UExecCalc_Damage_F::UExecCalc_Damage_F()
 {
 	//
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 }
 
 void UExecCalc_Damage_F::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -50,15 +54,40 @@ void UExecCalc_Damage_F::Execute_Implementation(const FGameplayEffectCustomExecu
 	EvaluateParameters.SourceTags = SourceTags;
 	EvaluateParameters.TargetTags = TargetTags;
 
-	float Armor = 0.f;
+
+	//Get Damage Set by Caller Magnitude(通知幅度)
+
+	//获取伤害数值
+	float Damage = Spec.GetSetByCallerMagnitude(FFuraGamePlayTags::Get().FDamage);
+	
+	//创建局部格挡几率变量
+	float TargetBlockChance=0.f;
+
+	//捕获格挡值并设置到TargetBlockChance ---捕获到的值是0，也就是说原因在于这个值没有设置正确。
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluateParameters,
+	                                                           TargetBlockChance);
+	TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.f); //返回两个值中值更大的值，防止出现负数的存在
+
+	//创建bool变量，(随机生成1~100之间的数值，然后判断格挡几率是否大于随机的数值，数值大于就是格挡成功
+	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	if (bBlocked)
+	{
+		//格挡成功，伤害减半。这个是这个教程的计算方法。（这个应该类似闪避，闪避概率应该时完全逃避掉伤害）)
+		Damage = Damage / 2.f;
+	}
+
+
+	/*float Armor = 0.f;
 	//尝试计算固定属性幅度
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
 	//确定Armor不为负数(限制范围)
 	Armor = FMath::Max<float>(0.f, Armor);
 
 	++Armor;
+	*/
 
-	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
-	
+	const FGameplayModifierEvaluatedData EvaluatedData(UFuraAttributeSet::GetIncomingDamageAttribute(),
+	                                                   EGameplayModOp::Additive, Damage);
+
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
