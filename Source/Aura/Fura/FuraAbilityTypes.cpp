90e614ef456f83e28f25f6503fbb903c3a510b80
王鹏飞 <2468852146@qq.com>
2024-09-25 17:24:13 +0800

    13-3:18:03

---
+++ Source/Aura/Fura/FuraAbilityTypes.cpp
@@ -0,0 +1,7 @@
+﻿#include "FuraAbilityTypes.h"
+
+bool FFuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	//return FGameplayEffectContext::NetSerialize(Ar, Map, bOutSuccess);
+	return true;
+}
---
+++ Source/Aura/Fura/FuraAbilityTypes.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include "GameplayAbilities/Public/GameplayEffectTypes.h"
+#include "FuraAbilityTypes.generated.h"
+/*
+ * 创建自定义结构体
+ * FFuraGameplayEffectContext 继承自 FGameplayEffectContext
+ * 因此它拥有 FGameplayEffectContext 中的所有功能，并可以添加更多自定义的行为和数据。
+ */
+USTRUCT(BlueprintType)
+struct FFuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+	//返回是否暴击
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	//返回是否格挡
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+	//设置暴击状态
+	void SetCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	//设置格挡状态
+	void SetBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	//网络序列化，重写子类必须重写这个函数
+	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+	//是否阻挡
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	//是否暴击
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
